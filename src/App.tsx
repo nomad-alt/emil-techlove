308a65933758f5d380ab109b5bf2345837f2a81f
nomad-alt <nomad-alt@proton.me>
2023-07-03 17:24:38 +0200

    fetching data from repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { RepoData, fetchRepos } from './api/fetchRepos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [repoData, setRepoData] = useState<RepoData[]>([]);
+
+    useEffect(() => {
+        const fetchData = async (): Promise<void> => {
+            const data: RepoData[] = await fetchRepos();
+            setRepoData(data);
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        console.log(repoData);
+    }, [repoData]);
+
+    return (
+        <div>
+            <h1>GitHub Repos</h1>
+        </div>
+    );
+};
 
 export default App;
+
