afff6add2677d667a688c320220028c3085aafde
nomad-alt <nomad-alt@proton.me>
2023-07-03 19:35:02 +0200

    added global css

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { RepoData } from '../api/fetchRepos';
 import styles from './RepoCard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faStar } from '@fortawesome/free-solid-svg-icons';
 
 interface RepoCardProps {
     repo: RepoData;
 }
 
 const RepoCard: React.FC<RepoCardProps> = ({ repo }) => {
-    return (
-        <div className={styles['repo-card']}>
-            <h2>{repo.full_name}</h2>
-            <p>{repo.description}</p>
-            <p>Watchers: {repo.watchers}</p>
-            <p>Stargazers: {repo.stargazers_count}</p>
-            <a href={repo.homepage} target="_blank" rel="noopener noreferrer">Visit Homepage</a>
+  return (
+    <div className={styles.card}>
+      <h2>{repo.full_name}</h2>
+      <p>{repo.description}</p>
+      <div className={styles.stats}>
+        <div className={styles.stat}>
+          <FontAwesomeIcon icon={faEye} />
+          <p>{repo.watchers}</p>
         </div>
-    );
+        <div className={styles.stat}>
+          <FontAwesomeIcon icon={faStar} />
+          <p>{repo.stargazers_count}</p>
+        </div>
+      </div>
+      <a className={styles.link} href={repo.homepage} target="_blank" rel="noopener noreferrer">
+        Visit Homepage
+      </a>
+    </div>
+  );
 };
 
 export default RepoCard;
+
